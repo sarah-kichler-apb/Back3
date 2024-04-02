1057ea7b57126cac667f4d380bba91ef71a5e1c4
Sarah <sarah.kichler.estudante@petrobras.com.br>
2024-04-02 16:58:07 -0300

    Subindo o primeiro servidor web

---
+++ server1.js
@@ -0,0 +1,14 @@
+// Criando um servidor web usando node
+const http = require('http');
+
+const server = http.createServer((req,res)=>{
+    console.log('Pedido efetuado.');
+    res.setHeader('Content-type', 'text/html')
+    res.write('Pedido aceito. \n');
+    res.write('<h3>Pedido aceito dnv.</h3>');
+    res.end();
+});
+
+server.listen(3000, 'localHost', ()=>{
+    console.log('Servidor iniciado.');
+});
